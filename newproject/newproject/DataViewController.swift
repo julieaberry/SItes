807a96e4666d482d7c1b895598f12734070eafb6
Julie Berry <jberry@fullsail.edu>
2015-10-28 15:13:22 -0700

    site updates

---
+++ newproject/newproject/DataViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DataViewController.swift
+//  newproject
+//
+//  Created by Julie Berry on 10/20/15.
+//  Copyright (c) 2015 Julie Berry. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: AnyObject?
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        if let obj: AnyObject = dataObject {
+            self.dataLabel!.text = obj.description
+        } else {
+            self.dataLabel!.text = ""
+        }
+    }
+
+
+}
+
